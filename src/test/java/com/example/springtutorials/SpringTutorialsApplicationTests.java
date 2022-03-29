edefae9dd11fe2080aa1ddf906f932a096a61672
MikesMikes <michaelkaliber15@gmail.com>
2022-03-29 18:22:18 +0100

    initial commit

---
+++ src/test/java/com/example/springtutorials/SpringTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
