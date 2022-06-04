7f00f788652fea1c7d88f542cf860d6d9a4bdd3c
MikesMikes <michaelkaliber15@gmail.com>
2022-06-04 02:55:15 +0100

    created publisher controller closes #17

--- src/main/java/mikesmikes/github/bookpublishing/controllers/AuthorController.java
+++ src/main/java/mikesmikes/github/bookpublishing/controllers/AuthorController.java
@@ -1,10 +1,12 @@
 package mikesmikes.github.bookpublishing.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import mikesmikes.github.bookpublishing.services.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class AuthorController {
 
@@ -16,9 +18,11 @@ public class AuthorController {
 
     @RequestMapping("/author/index")
     public String getIndex(Model model) {
+        log.info("getIndex - ");
 
         model.addAttribute("authors", authorService.findAll());
 
+        log.info("getIndex - end");
         return "author/index";
     }
 
--- src/main/java/mikesmikes/github/bookpublishing/controllers/PublisherController.java
+++ src/main/java/mikesmikes/github/bookpublishing/controllers/PublisherController.java
@@ -1,2 +1,29 @@
-package mikesmikes.github.bookpublishing.controllers;public class PublisherController {
+package mikesmikes.github.bookpublishing.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import mikesmikes.github.bookpublishing.services.PublisherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @GetMapping("/publisher/findall")
+    public String findAll(Model model){
+        log.info("findAll - ");
+
+        model.addAttribute("publishers", publisherService.findAll());
+
+
+        log.info("findAll - end");
+        return "publisher/findall";
+    }
 }
